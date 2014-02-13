de20c84e605d7884f8a6eb449ca68a15d1da82db
Su Gi Chandran <sollercoaster@hotmail.com>
2014-02-13 18:43:31 +0800

    Added schema
    Added database schema used to create the tables.

---
+++ schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE users (
+  userID INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(20),
+  email VARCHAR(30),
+  password VARCHAR(255),
+  PRIMARY KEY  (`userID`)
+);
+
+CREATE TABLE `posts` (
+  postID INT NOT NULL AUTO_INCREMENT,
+  date_created TIMESTAMP,
+  title VARCHAR(255),
+  content TEXT,
+  userID INT,
+  PRIMARY KEY  (`postID`)
+);
+
+ALTER TABLE posts ADD CONSTRAINT posts_fk1 FOREIGN KEY (userID) REFERENCES Users(userID);
